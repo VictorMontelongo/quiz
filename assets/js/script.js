a0bbc303f4480ecb8aff4a25625772a913e382af
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-09 18:54:06 -0500

    uploading work, need to add scoreboard and final results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // establish global variables
 
-// This should display win lose cons
+// This should display win lose condition
 var winLoss = document.querySelector(".win-loss");
 
 // Start button
@@ -9,8 +9,7 @@ var startBtn = document.querySelector(".start-button");
 var scoreboard = document.querySelector(".scoreboard");
 // Score total
 var totalScore = document.querySelector(".total-score");
-// Answer Options
-var ansBtn = document.querySelector(".answer-options");
+var score = 0;
 
 var isWin = false;
 
@@ -19,29 +18,31 @@ var timerElement = document.querySelector(".timer-text");
 var timer;
 var timerCount;
 
-// question and answer variables... at first I atttempted to make a querselector but I tried a group array instead
-var quesSelection = document.querySelector("#question");
-var optionSelection = document.querySelector("#options");
+// question and answer variables
+var quesSelection = document.querySelector(".questions");
+var quesIndex = 0;
+var score = 0;
+
 var questions = [{
   question: "Where is the largest pyramid located in the world",
-  options: ["Mexico", "Eygpts", "Peru", "China"],
+  ansoptions: ["Mexico", "Eygpt", "Peru", "China"],
   answer: "Mexico",
 },
 {
   question: " This word “Minne” derives from a word in the Dakota language meaning what? ",
-  options: ["land", "grass", "water", "lake"],
+  ansoptions: ["land", "grass", "water", "lake"],
   answer: "water",
 
 },
 {
   question: "Lift Bridge, Boom Island, and Canal Park are all Minnesota companies that focus on the production of what?",
-  options: ["lumber", "beer", "corn", "apples"],
+  ansoptions: ["lumber", "beer", "corn", "apples"],
   answer: "beer"
 
 },
 {
   question: "What was the last name at birth of legendary Minnesota musician Prince? ",
-  options: ["Nelson", "Johnson", "Smith", "Wilson"],
+  ansoptions: ["Nelson", "Johnson", "Smith", "Wilson"],
   answer: "Nelson"
 
 }
@@ -51,16 +52,18 @@ var questions = [{
 
 function startGame() {
   isWin = false;
-  timerCount = 5;
+  timerCount = 60;
   // this should prevent the user from pressing the start button while the quiz is in progress  
   startBtn.disabled = true;
   startTimer()
+
 }
 
 //  my time starter using elements from mini project guess game
 function startTimer() {
   // Sets timer
-  timer = setInterval(function () {
+  var timer = setInterval(function () {
+    questionRender()
     timerCount--;
     timerElement.textContent = timerCount;
     if (timerCount >= 0) {
@@ -82,19 +85,42 @@ function startTimer() {
 
 function loseGame() {
   winLoss.textContent = "GAME OVER";
-  startButton.disabled = false;
-
+  startBtn.disabled = false;
 }
-function wingame() {
+function winGame() {
   winLoss.textContent = "Nice"
   startBtn.disabled = false;
 }
+// This function should work with function startGame with the Array of questions
+function questionRender() {
+  var currentQues = questions[quesIndex];
+  // var pTag = document.createElement("p");
+  quesSelection.textContent = currentQues.question
+  for (var i = 0; i < currentQues.ansoptions.length; i++) {
+    var ansoptions = currentQues.ansoptions[i]
+    var div = document.createElement("div")
+    var btn = document.createElement("button")
+    btn.textContent = ansoptions
+    btn.answer = quesSelection.answer
+    btn.addEventListener("click", function (event) { Answerlistener(event.target, currentQues.answer); }, false)
+    div.appendChild(btn)
+    quesSelection.appendChild(div)
+  }
+}
+//how to make the correct choice can be made
+function Answerlistener(btn, answer) {
+  console.log(btn.textContent, answer)
+  if (btn.textContent === answer) {
+    checkAnswer()
+  }
+  questionRender()
+  quesIndex++
+}
 
-
-
-
-
-
+function checkAnswer(userAnswer) {
+  if (userAnswer === questions.answer)
+    score = score + 1;
+}
 
 
 
